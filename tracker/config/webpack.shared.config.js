2c6f633d3391fb3b77032452360a039544ebda1b
tony chang <tony@ponderer.org>
2018-07-16 17:18:54 -0700

    Add favicon based on chart emoji.

--- tracker/config/webpack.shared.config.js
+++ tracker/config/webpack.shared.config.js
@@ -30,8 +30,8 @@ module.exports = (options) => ({
       loader: 'file-loader',
     }, {
       test: /\.(jpg|png|gif)$/,
-        include: options.srcs,
-        loaders: [
+      include: options.srcs,
+      loaders: [
         'file-loader',
         'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}',
       ],
