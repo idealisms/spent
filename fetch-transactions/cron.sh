b227ebd526ab2fcf9115a4351eaf4075186cd70b
tony chang <tony@ponderer.org>
2018-10-01 21:35:48 -0700

    Add a third chase account.

--- fetch-transactions/cron.sh
+++ fetch-transactions/cron.sh
@@ -4,5 +4,6 @@ cd `dirname $0`
 timeout -k 300 180 nodejs barclay.js
 timeout -k 300 180 nodejs chase.js
 timeout -k 300 180 nodejs chase.js chase2
+timeout -k 300 180 nodejs chase.js chase3
 
 timeout 60 nodejs import.js
