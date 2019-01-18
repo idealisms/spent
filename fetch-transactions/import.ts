93f408f7d995ef60698a16cdc7eddca9763d7c4d
tony chang <tony@ponderer.org>
2019-01-18 15:24:55 -0800

    Update Chase CSV file format.
    They reordered the row items and added a category.

--- fetch-transactions/import.ts
+++ fetch-transactions/import.ts
@@ -121,7 +121,7 @@ function normalizeIntoRows(fileAsString: string, filename:string): [string, stri
   }
   if (rows[0].startsWith('posted,') || rows[0].startsWith('forecasted,')) {
       return normalizeUSAA(rows);
-  } else if (rows[0].startsWith('Type,Trans Date,')) {
+  } else if (rows[0].startsWith('Transaction Date,Post Date,Description,Category,Type,Amount')) {
       // Skip the header row.
       return normalizeChase(rows.slice(1), filename);
   } else if (rows[0] == 'Barclays Bank Delaware') {
@@ -150,23 +150,16 @@ function normalizeChase(rows: string[], filename: string): [string, string, numb
   let cardId = match ? match[1] : '0000';
   let ret: [string, string, number, string, string][] = [];
   for (let row of rows) {
-      if (row.endsWith(',')) {
-          let nextToLastComma = row.lastIndexOf(',', row.length - 2);
-          row = row.substring(0, nextToLastComma);
-      }
-      // Chase's CSV file can have commas in the description and doesn't use quotes.
-      let tokens = row.split(',');
-      if (tokens.length > 5) {
-          tokens[3] = tokens.slice(3, tokens.length - 1).join(',');
-          tokens[4] = tokens[tokens.length - 1];
-          tokens = tokens.slice(0, 5);
-      }
-      let amount = Math.round(Number(tokens[4]) * -100);
-      if (Number.isNaN(amount)) {
-          console.log(tokens);
-          break;
-      }
-      ret.push([tokens[3], tokens[1], amount, 'chase' + cardId, row]);
+    let tokens = parseCSVline(row);
+    let amount = Math.round(Number(tokens[5]) * -100);
+    if (Number.isNaN(amount)) {
+        console.log(tokens);
+        break;
+    }
+    // [desc, date, amount, source, original line].
+    // Rewrite the row into the old format to avoid dupes.
+    let oldRow = [tokens[4], tokens[0], tokens[1], tokens[2], tokens[5]].join(',');
+    ret.push([tokens[2], tokens[0], amount, 'chase' + cardId, oldRow]);
   }
   return ret;
 }
