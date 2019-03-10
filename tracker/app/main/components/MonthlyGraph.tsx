cd3382ffc747008382952c1323951ab9ad568b30
tony chang <tony@ponderer.org>
2019-03-09 19:38:41 -0800

    Remove unused MonthlyGraph code.
    Some scroll logic got copied from DailyGraph, but
    since there's much less data in the monthly view,
    it's not needed.

--- tracker/app/main/components/MonthlyGraph.tsx
+++ tracker/app/main/components/MonthlyGraph.tsx
@@ -12,7 +12,6 @@ const styles = (theme: Theme) => createStyles({
     '@media (max-width: 420px)': {
       paddingBottom: '0',
     },
-    overflowX: 'auto',
   },
 });
 interface IMonthlyGraphProps extends WithStyles<typeof styles> {
@@ -23,69 +22,50 @@ interface IMonthlyGraphState {
 }
 const MonthlyGraph = withStyles(styles)(
 class extends React.Component<IMonthlyGraphProps, IMonthlyGraphState> {
-  private container: HTMLElement|null = null;
-
-  public componentDidMount(): void {
-    if (!this.container) {
-      console.log('container not set (componentDidMount)');
-      return;
-    }
-    this.showEndOfGraph();
-  }
 
   public render(): React.ReactElement<object> {
     let classes = this.props.classes;
 
     return (
-        <div className={classes.root} ref={(elt) => this.container = elt}>
-          <div style={{width: 'auto', height: '100%'}}>
-            <Chart
-                chartType='ComboChart'
-                columns={[
-                  {label: 'Date', type: 'date'},
-                  {label: 'Spent', type: 'number'},
-                  {label: 'Balance', type: 'number'},
-                ]}
-                rows={this.props.data as GoogleDataTableCell[][]}
-                options={{
-                  chartArea: {
-                    top: 16,
-                    right: 16,
-                    bottom: 32,
-                    left: 80,
+        <div className={classes.root}>
+          <Chart
+              chartType='ComboChart'
+              columns={[
+                {label: 'Date', type: 'date'},
+                {label: 'Spent', type: 'number'},
+                {label: 'Balance', type: 'number'},
+              ]}
+              rows={this.props.data as GoogleDataTableCell[][]}
+              options={{
+                chartArea: {
+                  top: 16,
+                  right: 16,
+                  bottom: 32,
+                  left: 80,
+                },
+                seriesType: 'bars',
+                series: { 1: { type: 'line' } },
+                hAxis: {
+                  title: '',
+                  textStyle: {
+                    fontSize: 13,
                   },
-                  seriesType: 'bars',
-                  series: { 1: { type: 'line' } },
-                  hAxis: {
-                    title: '',
-                    textStyle: {
-                      fontSize: 13,
-                    },
+                },
+                vAxis: {
+                  title: '',
+                  format: '¤#,###',
+                  textPosition: 'out',
+                  textStyle: {
+                    fontSize: 16,
                   },
-                  vAxis: {
-                    title: '',
-                    format: '¤#,###',
-                    textPosition: 'out',
-                    textStyle: {
-                      fontSize: 16,
-                    },
-                  },
-                  legend: {position: 'none'},
-                }}
-                graph_id={this.props.graph_id}
-                width='auto'
-                height='100%'
-              />
-          </div>
+                },
+                legend: {position: 'none'},
+              }}
+              graph_id={this.props.graph_id}
+              width='auto'
+              height='100%'
+            />
         </div>);
-  }
-
-  private showEndOfGraph = () => {
-    if (!this.container) {
-      console.log('container not set (showEndOfGraph)');
-      return;
-    }
-    this.container.scrollLeft = this.container.scrollWidth;
   }
 });
 
