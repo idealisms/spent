8e6006b7765ec2df57d8de31f34859df31f844b4
tony chang <tony@ponderer.org>
2019-03-13 22:32:05 -0700

    Fix location of path in serviceWorker.ts.
    The intent was for PUBLIC_URL to be the domain.

--- tracker/app/serviceWorker.ts
+++ tracker/app/serviceWorker.ts
@@ -31,7 +31,7 @@ type Config = {
 
 export function register(config?: Config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    let PUBLIC_URL = process.env.PUBLIC_URL || '/';
+    let PUBLIC_URL = process.env.PUBLIC_URL || '';
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(
       PUBLIC_URL,
@@ -45,7 +45,7 @@ export function register(config?: Config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${PUBLIC_URL}service-worker.js`;
+      const swUrl = `${PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
