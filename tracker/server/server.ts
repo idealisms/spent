f0118982a14ed364a97f22662ba54385c15598e0
tony chang <tony@ponderer.org>
2020-07-23 16:56:07 -0700

    Have the server serve the static files.
    Properly serve the single page app. Include
    some future code for server side routing.

--- tracker/server/server.ts
+++ tracker/server/server.ts
@@ -1,9 +1,36 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, send } from "https://deno.land/x/oak/mod.ts";
+
+// Valid paths for our single page app.
+const paths = [
+  '/daily',
+  '/monthly',
+  '/editor',
+  '/report',
+];
 
 const app = new Application();
+// For future server side handling.
+// import { Router }
+// const router = new Router();
+
+// router.get('/hello', (ctx) => {
+//   ctx.response.body = 'Hello World!';
+// });
+
+// app.use(router.routes());
+// app.use(router.allowedMethods());
+
+app.use(async (ctx) => {
+  const path = paths.indexOf(ctx.request.url.pathname) != -1 ? '/' : ctx.request.url.pathname;
+  await send(ctx, path, {
+    root: './build',
+    index: "index.html",
+  });
+});
+
 
 app.use((ctx) => {
-  ctx.response.body = "Hello World!";
+  ctx.response.body = "not found";
 });
 
 await app.listen({ port: 8000 });
