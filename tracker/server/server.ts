e9173b5f26c5b3315294d25ec97742d205988930
tony chang <tony@ponderer.org>
2020-07-29 13:40:15 -0700

    Set up for live reloading.

--- tracker/server/server.ts
+++ tracker/server/server.ts
@@ -8,6 +8,8 @@ const paths = [
   '/report',
 ];
 
+const isDev = Deno.env.get('NODE_ENV') == 'development';
+
 const app = new Application();
 // For future server side handling.
 // import { Router }
@@ -22,6 +24,7 @@ const app = new Application();
 
 app.use(async (ctx) => {
   const path = paths.indexOf(ctx.request.url.pathname) != -1 ? '/' : ctx.request.url.pathname;
+  // TODO: If isDev, inject JS into the index.html file.
   await send(ctx, path, {
     root: './build',
     index: 'index.html',
